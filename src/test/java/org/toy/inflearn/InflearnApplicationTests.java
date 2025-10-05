f3a701a03ac2693f14b7cc2dc2b7f1c83a901a07
mary-code217 <xorl0514@naver.com>
2025-10-05 22:59:32 +0900

    chore: 프로젝트 초기 설정

---
+++ src/test/java/org/toy/inflearn/InflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.toy.inflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
