bc5e05fa29fefa7dfbc7f3c0cec0ed588bc7653c
mary-code217 <xorl0514@naver.com>
2025-10-09 22:55:56 +0900

    member 도메인 구현중 - #4

--- src/main/java/org/toy/inflearn/Member.java
+++ src/main/java/org/toy/inflearn/Member.java
@@ -3,6 +3,7 @@ package org.toy.inflearn;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -18,7 +19,30 @@ public class Member {
 
     private MemberStatus status;
 
+    public Member(String email, String nickname, String passwordHash, MemberStatus status) {
+        this.email = email;
+        this.nickname = nickname;
+        this.passwordHash = passwordHash;
+        this.status = status;
+    }
 
+    public static Member register() {
+        Member member = new Member();
+
+        return member;
+    }
+
+    public void activate() {
+        Assert.state(this.status == MemberStatus.PENDING, "PENDING 상태가 아닙니다.");
+
+        this.status = MemberStatus.ACTIVE;
+    }
+
+    public void deactivate() {
+        Assert.state(this.status == MemberStatus.ACTIVE, "ACTIVE 상태가 아닙니다.");
+
+        this.status = MemberStatus.DEACTIVATED;
+    }
 
 
 }
