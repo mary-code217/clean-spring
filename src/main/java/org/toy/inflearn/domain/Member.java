ff61a368b4504fc02b264bb1864de6e223fc9a72
mary-code217 <xorl0514@naver.com>
2025-10-16 23:30:40 +0900

    chore: orm.xml 설정 추가

--- src/main/java/org/toy/inflearn/domain/Member.java
+++ src/main/java/org/toy/inflearn/domain/Member.java
@@ -12,24 +12,26 @@ import static java.util.Objects.requireNonNull;
 import static org.springframework.util.Assert.state;
 
 @Entity
+@Table(name = "MEMBER", uniqueConstraints =
+        @UniqueConstraint(name = "UK_MEMBER_EMAIL_ADDRESS", columnNames = "email_address")
+)
 @Getter
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @NaturalIdCache
-public class Member {
-
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+public class Member extends AbstractEntity {
     @Embedded
     @NaturalId
     private Email email;
 
+    @Column(length = 100, nullable = false)
     private String nickname;
 
+    @Column(length = 200, nullable = false)
     private String passwordHash;
 
     @Enumerated(EnumType.STRING)
+    @Column(length = 50, nullable = false)
     private MemberStatus status;
 
     public static Member register(MemberRegisterRequest registerRequest, PasswordEncoder passwordEncoder) {
