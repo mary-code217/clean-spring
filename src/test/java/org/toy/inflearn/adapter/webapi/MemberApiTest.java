95d4137a65101f6754d483620db458c705fcbe1b
mary-code217 <xorl0514@naver.com>
2025-10-21 22:35:04 +0900

    예외처리 - #17

--- src/test/java/org/toy/inflearn/adapter/webapi/MemberApiTest.java
+++ src/test/java/org/toy/inflearn/adapter/webapi/MemberApiTest.java
@@ -8,12 +8,16 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.json.JsonPathValueAssert;
+import org.springframework.test.web.servlet.MockMvcResultHandlersDsl;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
 import org.springframework.test.web.servlet.assertj.MvcTestResult;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.transaction.annotation.Transactional;
 import org.toy.inflearn.adapter.webapi.dto.MemberRegisterResponse;
+import org.toy.inflearn.application.member.provided.MemberRegister;
 import org.toy.inflearn.application.member.required.MemberRepository;
 import org.toy.inflearn.domain.member.Member;
 import org.toy.inflearn.domain.member.MemberFixture;
@@ -25,6 +29,7 @@ import java.util.function.Consumer;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.toy.inflearn.AssertThatUtils.equalsTo;
 import static org.toy.inflearn.AssertThatUtils.notNull;
 
@@ -36,6 +41,7 @@ public class MemberApiTest {
     final MockMvcTester mvcTester;
     final ObjectMapper objectMapper;
     final MemberRepository memberRepository;
+    final MemberRegister memberRegister;
 
     @Test
     void register() throws JsonProcessingException, UnsupportedEncodingException {
@@ -58,5 +64,20 @@ public class MemberApiTest {
         assertThat(member.getEmail().address()).isEqualTo(request.email());
         assertThat(member.getNickname()).isEqualTo(request.nickname());
         assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+    }
+
+    @Test
+    void 이메일_중복() throws JsonProcessingException {
+        memberRegister.register(MemberFixture.createMemberRegisterRequest());
+
+        MemberRegisterRequest request = MemberFixture.createMemberRegisterRequest();
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        MvcTestResult result = mvcTester.post().uri("/api/members").contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson).exchange();
+
+        assertThat(result)
+                .apply(print())
+                .hasStatus(HttpStatus.CONFLICT);
     }
 }
